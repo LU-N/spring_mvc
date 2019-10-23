46083524f4697920dd67c9d9448fac8ce54c4fad
JinLu <1124766160@qq.com>
2019-10-23 18:47:33 +0800

    no message

--- src/main/java/com/controller/UserController.java
+++ src/main/java/com/controller/UserController.java
@@ -1,17 +1,20 @@
 package com.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 
 /**
  * @author JinLu
  * @date 2019/10/22
  */
 @Controller
+@RequestMapping("/user")
 public class UserController {
-    @RequestMapping("/quick")
+    @GetMapping(value = "/quick", params = {"username"})
     public String save() {
         System.out.println("Controller save running");
-        return "success.jsp";
+        return "success";
     }
 }
